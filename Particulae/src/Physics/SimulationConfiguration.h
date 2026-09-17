8c50afbfb3e4dbafe10b96e4c12483a9247e15d5
Storm Buijs <stormbuijs@gmail.com>
2026-09-17 21:20:22 +0200

    Voeg centrale SimulationConfiguration toe

---
+++ Particulae/src/Physics/SimulationConfiguration.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "../Math/Real.h"
+
+
+
+// Centrale plek voor alle parameters,
+
+struct SimulationConfiguration
+{
+	// Morse-potentiaal
+
+	Real morseWellDepth = 50.0;
+	Real morseWidth = 0.3;
+	Real morseRestLength = 20.0;
+
+
+	// Coulomb
+
+	Real coulombConstant = 2000.0;
+	Real coulombSoftening = 5.0;
+
+
+	// Pauli-uitsluiting
+
+	Real pauliRepulsionStrength = 2000.0;
+	Real pauliRepulsionRadius = 8.0;
+
+
+	// Langevin-demping
+
+	Real langevinFriction = 0.5;
+	Real langevinNoise = 0.1;
+
+
+	// Elektronen
+
+	Real electronMass = 0.05;
+	Real electronRadius = 3.0;
+	Real electronCharge = -1.0;
+	Real electronPairDistance = 4.0;
+
+	Real pairMorseWellDepth = 20.0;
+	Real pairMorseWidth = 0.5;
+
+
+	// Nucleus
+
+	Real nucleusRadius = 12.0;
+
+
+	// Veiligheidsgrens
+
+	Real minimumDistance = 0.0001;
+};
